5b2faf4d918f1406d44dbea3e3a1c43cc5a499d8
Jacek Tomaszewski <jacek@jtom.me>
2018-01-25 18:30:44 +0100

    Implement workaround fix for safari

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,13 +1,23 @@
 import {
-  Component, Input, Output, ChangeDetectionStrategy, EventEmitter, ChangeDetectorRef
+  Component, Input, Output, ChangeDetectionStrategy, EventEmitter, ChangeDetectorRef, SimpleChanges, ElementRef, ViewChild
 } from '@angular/core';
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-input",
+  templateUrl: "./input.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InputComponent {
-  @Input() public value: string = '';
+  @Input() public value: string = "";
   @Output() public valueChange: EventEmitter<string> = new EventEmitter();
+
+  @ViewChild("input") private input: ElementRef;
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.value) {
+      if (this.input.nativeElement.value != this.value) {
+        this.input.nativeElement.value = this.value || "";
+      }
+    }
+  }
 }
